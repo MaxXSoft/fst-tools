8f5292c0b945b64a841d0559c6a0e6ba75e54db1
MaxXSoft <x@MaxXSoft.net>
2023-02-13 20:44:46 +0800

    (fstapi/utils) added helper trait `IntoCString`

--- fstapi/src/utils.rs
+++ fstapi/src/utils.rs
@@ -1,4 +1,21 @@
+use crate::{Error, Result};
+use std::ffi::CString;
 use std::num::NonZeroU32;
+
+/// Trait for converting bytes into [`CString`].
+pub(crate) trait IntoCString {
+  /// Converts into [`CString`].
+  fn into_cstring(self) -> Result<CString>;
+}
+
+impl<T> IntoCString for T
+where
+  T: Into<Vec<u8>>,
+{
+  fn into_cstring(self) -> Result<CString> {
+    CString::new(self).map_err(Error::CStringConv)
+  }
+}
 
 /// Trait for converting `Option<Handle>` into handle for C API.
 pub(crate) trait IntoCHandle {
