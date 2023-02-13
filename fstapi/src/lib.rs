d625e352421c3d47c1365dbff03288a735df5b7a
MaxXSoft <x@MaxXSoft.net>
2023-02-13 20:39:04 +0800

    (fstapi) added enum `Error` and type `Result`

--- fstapi/src/lib.rs
+++ fstapi/src/lib.rs
@@ -4,3 +4,29 @@ mod types;
 
 pub use consts::*;
 pub use types::*;
+
+use std::fmt;
+
+/// Error that may returned from FST-related APIs.
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum Error {
+  /// CString conversion error.
+  CStringConv(std::ffi::NulError),
+  /// Writer creation error.
+  WriterCreate,
+  /// Null handle.
+  NullHandle,
+}
+
+impl fmt::Display for Error {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match self {
+      Self::CStringConv(e) => write!(f, "CString conversion error, {e}"),
+      Self::WriterCreate => write!(f, "writer creation error"),
+      Self::NullHandle => write!(f, "null handle"),
+    }
+  }
+}
+
+/// Result that error type is [`Error`].
+pub type Result<T> = std::result::Result<T, Error>;
