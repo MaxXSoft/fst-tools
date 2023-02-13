89a22807d43e55da01a7d3387172e8308816c5fe
MaxXSoft <x@MaxXSoft.net>
2023-02-13 21:56:36 +0800

    (fstapi) added `Error::InvalidUtf8Str`

--- fstapi/src/lib.rs
+++ fstapi/src/lib.rs
@@ -13,6 +13,8 @@ use std::fmt;
 /// Error that may returned from FST-related APIs.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Error {
+  /// Invalid UTF-8 string.
+  InvalidUtf8Str,
   /// CString conversion error.
   CStringConv(std::ffi::NulError),
   /// Writer creation error.
@@ -24,6 +26,7 @@ pub enum Error {
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match self {
+      Self::InvalidUtf8Str => write!(f, "invalid UTF-8 string"),
       Self::CStringConv(e) => write!(f, "CString conversion error, {e}"),
       Self::WriterCreate => write!(f, "writer creation error"),
       Self::NullHandle => write!(f, "null handle"),
