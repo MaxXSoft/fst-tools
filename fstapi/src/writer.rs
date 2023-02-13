18e34370273c727502461ad0611c2c2156d5d3e9
MaxXSoft <x@MaxXSoft.net>
2023-02-13 22:28:12 +0800

    (fstapi/writer) fixed some details

--- fstapi/src/writer.rs
+++ fstapi/src/writer.rs
@@ -1,12 +1,12 @@
 use crate::consts::{AttrType, FileType, PackType, ScopeType, VarDir, VarType};
 use crate::types::Handle;
-use crate::utils::{IntoCHandle, IntoCString};
+use crate::utils::*;
 use crate::{capi, Error, Result};
 use std::os::raw;
-use std::os::unix::prelude::OsStrExt;
 use std::path::Path;
 
 /// FST waveform writer.
+#[derive(Debug)]
 pub struct Writer {
   /// Non-null context pointer.
   ctx: *mut raw::c_void,
@@ -18,87 +18,13 @@ impl Writer {
   where
     P: AsRef<Path>,
   {
-    let path = path.as_ref().as_os_str().as_bytes().into_cstring()?;
+    let path = path.to_str()?.into_cstring()?;
     let ctx = unsafe { capi::fstWriterCreate(path.as_ptr(), use_compressed_hier as raw::c_int) };
     if ctx.is_null() {
-      Err(Error::WriterCreate)
+      Err(Error::ContextCreate)
     } else {
       Ok(Self { ctx })
     }
-  }
-
-  /// Creates a new variable.
-  pub fn create_var(
-    &mut self,
-    ty: VarType,
-    dir: VarDir,
-    len: u32,
-    name: &str,
-    alias: Option<Handle>,
-  ) -> Result<Handle> {
-    let name = name.into_cstring()?;
-    Handle::new(unsafe {
-      capi::fstWriterCreateVar(self.ctx, ty, dir, len, name.as_ptr(), alias.into_handle())
-    })
-    .ok_or(Error::NullHandle)
-  }
-
-  /// Emits value change for the given handle.
-  pub fn emit_value_change(&mut self, handle: Handle, value: &[u8]) {
-    unsafe {
-      capi::fstWriterEmitValueChange(
-        self.ctx,
-        handle.into(),
-        value.as_ptr() as *const raw::c_void,
-      )
-    }
-  }
-
-  /// Emits vairable-length value change for the given handle.
-  pub fn emit_var_len_value_change(&mut self, handle: Handle, value: &[u8]) {
-    unsafe {
-      capi::fstWriterEmitVariableLengthValueChange(
-        self.ctx,
-        handle.into(),
-        value.as_ptr() as *const raw::c_void,
-        value.len() as u32,
-      )
-    }
-  }
-
-  /// Emits time change.
-  pub fn emit_time_change(&mut self, time: u64) {
-    unsafe { capi::fstWriterEmitTimeChange(self.ctx, time) }
-  }
-
-  /// Flushes the content of the current writer to file.
-  pub fn flush(&mut self) {
-    unsafe { capi::fstWriterFlushContext(self.ctx) }
-  }
-
-  /// Sets attribute begin.
-  pub fn set_attr_begin(&mut self, ty: AttrType, sub_ty: i32, name: &str, arg: u64) -> Result<()> {
-    let name = name.into_cstring()?;
-    unsafe { capi::fstWriterSetAttrBegin(self.ctx, ty, sub_ty, name.as_ptr(), arg) };
-    Ok(())
-  }
-
-  /// Sets attribute end.
-  pub fn set_attr_end(&mut self) {
-    unsafe { capi::fstWriterSetAttrEnd(self.ctx) }
-  }
-
-  /// Sets scope.
-  pub fn set_scope(&mut self, ty: ScopeType, name: &str, component: &str) -> Result<()> {
-    let name = name.into_cstring()?;
-    let component = component.into_cstring()?;
-    unsafe { capi::fstWriterSetScope(self.ctx, ty, name.as_ptr(), component.as_ptr()) };
-    Ok(())
-  }
-
-  /// Sets upscope.
-  pub fn set_upscope(&mut self) {
-    unsafe { capi::fstWriterSetUpscope(self.ctx) }
   }
 
   /// Sets comment.
@@ -163,6 +89,80 @@ impl Writer {
     let version = version.into_cstring()?;
     unsafe { capi::fstWriterSetVersion(self.ctx, version.as_ptr()) };
     Ok(self)
+  }
+
+  /// Sets attribute begin.
+  pub fn set_attr_begin(&mut self, ty: AttrType, sub_ty: i32, name: &str, arg: u64) -> Result<()> {
+    let name = name.into_cstring()?;
+    unsafe { capi::fstWriterSetAttrBegin(self.ctx, ty, sub_ty, name.as_ptr(), arg) };
+    Ok(())
+  }
+
+  /// Sets attribute end.
+  pub fn set_attr_end(&mut self) {
+    unsafe { capi::fstWriterSetAttrEnd(self.ctx) }
+  }
+
+  /// Sets scope.
+  pub fn set_scope(&mut self, ty: ScopeType, name: &str, component: &str) -> Result<()> {
+    let name = name.into_cstring()?;
+    let component = component.into_cstring()?;
+    unsafe { capi::fstWriterSetScope(self.ctx, ty, name.as_ptr(), component.as_ptr()) };
+    Ok(())
+  }
+
+  /// Sets upscope.
+  pub fn set_upscope(&mut self) {
+    unsafe { capi::fstWriterSetUpscope(self.ctx) }
+  }
+
+  /// Creates a new variable.
+  pub fn create_var(
+    &mut self,
+    ty: VarType,
+    dir: VarDir,
+    len: u32,
+    name: &str,
+    alias: Option<Handle>,
+  ) -> Result<Handle> {
+    let name = name.into_cstring()?;
+    Handle::new(unsafe {
+      capi::fstWriterCreateVar(self.ctx, ty, dir, len, name.as_ptr(), alias.into_handle())
+    })
+    .ok_or(Error::NullHandle)
+  }
+
+  /// Emits value change for the given handle.
+  pub fn emit_value_change(&mut self, handle: Handle, value: &[u8]) {
+    unsafe {
+      capi::fstWriterEmitValueChange(
+        self.ctx,
+        handle.into(),
+        value.as_ptr() as *const raw::c_void,
+      )
+    }
+  }
+
+  /// Emits vairable-length value change for the given handle.
+  pub fn emit_var_len_value_change(&mut self, handle: Handle, value: &[u8]) {
+    unsafe {
+      capi::fstWriterEmitVariableLengthValueChange(
+        self.ctx,
+        handle.into(),
+        value.as_ptr() as *const raw::c_void,
+        value.len() as u32,
+      )
+    }
+  }
+
+  /// Emits time change.
+  pub fn emit_time_change(&mut self, time: u64) {
+    unsafe { capi::fstWriterEmitTimeChange(self.ctx, time) }
+  }
+
+  /// Flushes the content of the current writer to file.
+  pub fn flush(&mut self) {
+    unsafe { capi::fstWriterFlushContext(self.ctx) }
   }
 }
 
