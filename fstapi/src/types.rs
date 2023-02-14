7f85780c26993b367dbb2c3dde2f7cca3db75683
MaxXSoft <x@MaxXSoft.net>
2023-02-14 11:07:25 +0800

    (fstapi/types) updated type `Handle`, removed `EnumHandle`

--- fstapi/src/types.rs
+++ fstapi/src/types.rs
@@ -1,4 +1,33 @@
+use crate::utils::IntoCHandle;
 use std::num::NonZeroU32;
 
-pub type EnumHandle = NonZeroU32;
-pub type Handle = NonZeroU32;
+/// Defines a new handle type.
+macro_rules! def_handle_type {
+  ($name:ident) => {
+    pub struct $name(pub(crate) NonZeroU32);
+
+    impl $name {
+      /// Creates a new handle.
+      pub(crate) fn new(n: u32) -> Option<Self> {
+        NonZeroU32::new(n).map(Self)
+      }
+    }
+
+    impl From<$name> for u32 {
+      fn from(value: $name) -> Self {
+        value.0.into()
+      }
+    }
+
+    impl IntoCHandle for Option<$name> {
+      fn into_handle(self) -> u32 {
+        match self {
+          Some(i) => i.into(),
+          None => 0,
+        }
+      }
+    }
+  };
+}
+
+def_handle_type!(Handle);
