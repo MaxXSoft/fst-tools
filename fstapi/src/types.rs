f931a20356048f6202d4af3643f56f0c55266b63
MaxXSoft <x@MaxXSoft.net>
2023-02-15 15:27:37 +0800

    (fstapi/types) simplified the implementation

--- fstapi/src/types.rs
+++ fstapi/src/types.rs
@@ -1,36 +1,30 @@
 use crate::utils::IntoCHandle;
 use std::num::NonZeroU32;
 
-/// Defines a new handle type.
-macro_rules! def_handle_type {
-  ($name:ident) => {
-    #[repr(transparent)]
-    #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-    pub struct $name(pub(crate) NonZeroU32);
+/// Handle type, which is actually a 32-bit non-zero unsigned integer.
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Handle(pub(crate) NonZeroU32);
 
-    impl $name {
-      /// Creates a new handle.
-      pub(crate) fn new(n: u32) -> Option<Self> {
-        NonZeroU32::new(n).map(Self)
-      }
-    }
-
-    impl From<$name> for u32 {
-      /// Creates a non-zero unsigned integer from the handle.
-      fn from(value: $name) -> Self {
-        value.0.into()
-      }
-    }
-
-    impl IntoCHandle for Option<$name> {
-      fn into_handle(self) -> u32 {
-        match self {
-          Some(i) => i.into(),
-          None => 0,
-        }
-      }
-    }
-  };
+impl Handle {
+  /// Creates a new handle.
+  pub(crate) fn new(n: u32) -> Option<Self> {
+    NonZeroU32::new(n).map(Self)
+  }
 }
 
-def_handle_type!(Handle);
+impl From<Handle> for u32 {
+  /// Creates a non-zero unsigned integer from the handle.
+  fn from(value: Handle) -> Self {
+    value.0.into()
+  }
+}
+
+impl IntoCHandle for Option<Handle> {
+  fn into_handle(self) -> u32 {
+    match self {
+      Some(i) => i.into(),
+      None => 0,
+    }
+  }
+}
