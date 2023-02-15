2438e8b956a332abaa6d9c08386675edbe1f7c6f
MaxXSoft <x@MaxXSoft.net>
2023-02-15 16:40:18 +0800

    (findfst/find) moved `find_value` to module `find`

--- findfst/src/find.rs
+++ findfst/src/find.rs
@@ -1,3 +1,8 @@
+use crate::checker::{DenseChecker, DenseOnceChecker, SparseChecker, SparseOnceChecker};
+use crate::checker::{VarChecker, VarInfo};
+use crate::matcher::{ExactMatcher, RegexHexMatcher, RegexMatcher, ValueMatcher};
+use crate::printer::{FullPrinter, NamePrinter, Printer};
+use fstapi::{Handle, Reader, Result};
 use regex::{bytes::Regex, Error as RegexError};
 use std::fmt;
 
@@ -25,7 +30,7 @@ pub enum MatchInfo {
 }
 
 impl MatchInfo {
-  pub fn new(value: String, hex: bool, regex: bool) -> Result<Self, Error> {
+  pub fn new(value: String, hex: bool, regex: bool) -> std::result::Result<Self, Error> {
     if regex {
       let re = Regex::new(&value).map_err(Error::Regex)?;
       Ok(Self::Regex(re, hex))
@@ -46,6 +51,143 @@ impl MatchInfo {
       Err(Error::InvalidBin(value))
     } else {
       Ok(Self::Exact(value.into_bytes().into()))
+    }
+  }
+}
+
+/// Finds the matching value in the given FST waveform.
+pub fn find_value(
+  reader: &mut Reader,
+  value_match: MatchInfo,
+  vars: VarInfo,
+  all_matches: bool,
+  names_only: bool,
+) -> Result<()> {
+  match value_match {
+    MatchInfo::Regex(re, true) => {
+      find_value_m(reader, RegexMatcher::new(re), vars, all_matches, names_only)
+    }
+    MatchInfo::Regex(re, false) => find_value_m(
+      reader,
+      RegexHexMatcher::new(re),
+      vars,
+      all_matches,
+      names_only,
+    ),
+    MatchInfo::Exact(e) => {
+      find_value_m(reader, ExactMatcher::new(e), vars, all_matches, names_only)
+    }
+  }
+}
+
+/// Stage #2, with value matcher applied, determines variable checker.
+fn find_value_m<M>(
+  reader: &mut Reader,
+  value_matcher: M,
+  vars: VarInfo,
+  all_matches: bool,
+  names_only: bool,
+) -> Result<()>
+where
+  M: ValueMatcher,
+{
+  match (vars, all_matches) {
+    (VarInfo::Map(vars), false) => {
+      find_value_mc(reader, value_matcher, SparseChecker::new(vars), names_only)
+    }
+    (VarInfo::Map(vars), true) => find_value_mc(
+      reader,
+      value_matcher,
+      SparseOnceChecker::new(vars),
+      names_only,
+    ),
+    (VarInfo::Array(vars), false) => {
+      find_value_mc(reader, value_matcher, DenseChecker::new(vars), names_only)
+    }
+    (VarInfo::Array(vars), true) => find_value_mc(
+      reader,
+      value_matcher,
+      DenseOnceChecker::new(vars),
+      names_only,
+    ),
+  }
+}
+
+/// Stage #3, with value matcher and variable checker applied, determines printer.
+fn find_value_mc<M, T, C>(
+  reader: &mut Reader,
+  value_matcher: M,
+  var_checker: C,
+  names_only: bool,
+) -> Result<()>
+where
+  M: ValueMatcher,
+  C: VarChecker<T> + Clone,
+{
+  if names_only {
+    find_value_mcp(reader, value_matcher, var_checker, NamePrinter)
+  } else {
+    find_value_mcp(reader, value_matcher, var_checker, FullPrinter)
+  }
+}
+
+/// Final stage, all generics are applied, creates callbacks and
+/// finds for matching values.
+fn find_value_mcp<M, T, C, P>(
+  reader: &mut Reader,
+  value_matcher: M,
+  var_checker: C,
+  printer: P,
+) -> Result<()>
+where
+  M: ValueMatcher,
+  C: VarChecker<T> + Clone,
+  P: Printer,
+{
+  let mut var_checker = var_checker;
+  let mut var_checker2 = var_checker.clone();
+  let callback = |time, handle, value: &[u8]| {
+    find_value_callback(
+      &value_matcher,
+      &mut var_checker,
+      &printer,
+      time,
+      handle,
+      value,
+    )
+  };
+  let callback2 = |time, handle, value: &[u8]| {
+    find_value_callback(
+      &value_matcher,
+      &mut var_checker2,
+      &printer,
+      time,
+      handle,
+      value,
+    )
+  };
+  reader.for_each_block(callback, callback2)
+}
+
+/// Callback of FST block iterator.
+/// Runs value matcher, variable checker and printer.
+fn find_value_callback<M, T, C, P>(
+  value_matcher: &M,
+  var_checker: &mut C,
+  printer: &P,
+  time: u64,
+  handle: Handle,
+  value: &[u8],
+) where
+  M: ValueMatcher,
+  C: VarChecker<T>,
+  P: Printer,
+{
+  // Check if value matches.
+  if value_matcher.is_match(value) {
+    // Check the current variable and print.
+    if let Some(name) = var_checker.check(handle) {
+      printer.print(time, name, value);
     }
   }
 }
