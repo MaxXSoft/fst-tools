7fbfbb062ac75fb708c73e0ec9f18f0fc84771fe
MaxXSoft <x@MaxXSoft.net>
2023-02-17 09:30:54 +0800

    (readfst/section) changed trait definitions

--- readfst/src/section.rs
+++ readfst/src/section.rs
@@ -1,11 +1,11 @@
-use std::iter;
+use tabled::builder::Builder;
 use tabled::object::Rows;
 use tabled::{Alignment, Modify, Panel, Style, Table, Tabled};
 
 /// Item that represents the content of a [`Section`].
-pub enum Item<T> {
-  One(T),
-  Many(Vec<T>),
+pub enum Item<'a, T> {
+  One(&'a T),
+  Many(&'a [T]),
 }
 
 /// Section of waveform information.
@@ -14,45 +14,72 @@ pub trait Section {
   type Item;
 
   /// Returns section name.
-  fn name(&self) -> &str;
+  fn name() -> &'static str;
 
   /// Returns the item in the current section.
-  fn item(&mut self) -> Item<&Self::Item>;
+  fn item(&self) -> Item<Self::Item>;
 }
 
-/// Trait for printing a sections.
-pub trait Print {
-  /// Prints to stdout.
-  fn print(&mut self);
+/// Trait for converting to [`Table`].
+pub trait ToTable {
+  /// Converts to table.
+  fn to_table(&self) -> Table;
 }
 
-impl<S, I> Print for S
+impl<'a, T> ToTable for Item<'a, T>
+where
+  T: Tabled,
+{
+  fn to_table(&self) -> Table {
+    match self {
+      Item::One(i) => {
+        let mut builder = Table::builder([i]).index();
+        builder.set_index(0).transpose();
+        builder.build()
+      }
+      Item::Many(is) if is.is_empty() => {
+        let mut builder = Builder::default();
+        builder.add_record(["None"]);
+        builder.build()
+      }
+      Item::Many(is) => Table::new(*is),
+    }
+  }
+}
+
+impl<S, I> ToTable for S
 where
   S: Section<Item = I>,
   I: Tabled,
 {
-  fn print(&mut self) {
-    let mut table = match self.item() {
-      Item::One(i) => {
-        let mut builder = Table::builder(iter::once(i)).index();
-        builder.set_index(0).transpose();
-        builder.build()
-      }
-      Item::Many(is) => Table::new(is),
-    };
-    println!(
-      "{}",
-      table
-        .with(Style::modern())
-        .with(Panel::header(self.name()))
-        .with(Modify::new(Rows::first()).with(Alignment::center()))
-    );
+  fn to_table(&self) -> Table {
+    let mut table = self.item().to_table();
+    table
+      .with(Panel::header(Self::name()))
+      .with(Modify::new(Rows::first()).with(Alignment::center()));
+    table
+  }
+}
+
+/// Trait for printing to stdout.
+pub trait Print {
+  /// Prints to stdout.
+  fn print(&self);
+}
+
+impl<T> Print for T
+where
+  T: ToTable,
+{
+  fn print(&self) {
+    let mut table = self.to_table();
+    println!("{}", table.with(Style::modern()));
   }
 }
 
 impl Print for Vec<Box<dyn Print>> {
-  fn print(&mut self) {
-    for (i, s) in self.iter_mut().enumerate() {
+  fn print(&self) {
+    for (i, s) in self.iter().enumerate() {
       if i != 0 {
         println!()
       }
